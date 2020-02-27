1a56a19f3ea042c29380439102a74b37f7bfd7fc
Robson75 <rsamuelsson@gmail.com>
2020-02-27 18:36:29 +0100

    Make dice roll a few times each throw.

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.android.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
+private const val TAG = "MainActivity"
+
 class MainActivity : AppCompatActivity() {
+
     lateinit var diceImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,18 +26,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun rollDice(){
-        val randomInt = Random.nextInt(6) + 1
-        val drawableResource = when(randomInt){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.empty_dice
+    private fun rollDice() {
+        val minTurns = 2
+        val turns = Random.nextInt(10) + minTurns
+        var last = 0
+        Handler().postDelayed({
+            getNewDiceImage(turns, last)
+            Log.d(TAG, "get new dice image")
+
+        }, 100)
+
+    }
+
+    private fun getNewDiceImage(turns:Int, last:Int) {
+        var lastValue = last
+        if (turns > 0) {
+            var newValue = lastValue
+            while (newValue == lastValue) {
+                newValue = Random.nextInt(6) + 1
+            }
+            lastValue = newValue
+            val drawableResource = when (newValue) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.empty_dice
+            }
+            diceImage.setImageResource(drawableResource)
+            Handler().postDelayed({
+                getNewDiceImage(turns - 1, lastValue)
+                Log.d(TAG, "get new dice image")
+
+            }, 150)
         }
-        diceImage.setImageResource(drawableResource)
     }
 
 }
